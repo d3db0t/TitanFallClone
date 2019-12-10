a9745b1342b181b378d020ecd080cbdfe1c7d66f
Ahmed Hatem <ahmedhatem777@hotmail.com>
2019-12-11 01:55:31 +0200

    [+] Added intial titan shield design

--- Assets/Scripts/TitanShooting.cs
+++ Assets/Scripts/TitanShooting.cs
@@ -2,22 +2,17 @@
 using System.Collections.Generic;
 using UnityEngine;
 using UnityStandardAssets.Characters.FirstPerson;
-using TMPro;
 
 public class TitanShooting : MonoBehaviour
 {
     public int Damage;
     public float FireRate;
-    //public bool IsAuto;
     public float Range;
-    public GameObject Player;
     public Camera Camera;
     private float NextTimeToFire;
     public GameObject MetalImpactEffect;
     public GameObject ConcreteImpactEffect;
     public ParticleSystem MuzzleFlash;
-    //public GameObject Flash;
-    //public bool ToggleFlash;
 
     void Start()
     {
@@ -37,13 +32,10 @@ public class TitanShooting : MonoBehaviour
 
     public void Shoot()
     {
-        //MuzzleFlash.Play();
-        Instantiate(MuzzleFlash,
-                    new Vector3(transform.position.x,transform.position.y, transform.position.z + 3),
-                    Quaternion.identity);
-
+        MuzzleFlash.Play();
+        //CurrentMagazineSize -= 1;
         RaycastHit hit;
-        if(Physics.Raycast(Camera.transform.position, Camera.transform.forward, out hit, Range))
+        if (Physics.Raycast(Camera.transform.position, Camera.transform.forward, out hit, Range))
         {
             Debug.Log(hit.transform.name);
             if (hit.transform.tag == "Enemy")
@@ -51,6 +43,15 @@ public class TitanShooting : MonoBehaviour
                 if (!hit.transform.GetComponent<EnemyPilotController>().Dead)
                 {
                     hit.transform.GetComponent<EnemyPilotController>().TakeDamage(Damage);
+                    GameObject ImpactGO = Instantiate(MetalImpactEffect, hit.point, Quaternion.LookRotation(hit.normal));
+                    Destroy(ImpactGO, 2f);
+                }
+            }
+            else if (hit.transform.tag == "EnemyTitan")
+            {
+                if (!hit.transform.GetComponent<EnemyTitanController>().Dead)
+                {
+                    hit.transform.GetComponent<EnemyTitanController>().TakeDamage(Damage);
                     GameObject ImpactGO = Instantiate(MetalImpactEffect, hit.point, Quaternion.LookRotation(hit.normal));
                     Destroy(ImpactGO, 2f);
                 }
