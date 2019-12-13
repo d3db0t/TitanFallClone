91e21fe8e596a6229312aa77ba9c7fab90a528df
Ahmed Hatem <ahmedhatem777@hotmail.com>
2019-12-13 20:54:20 +0200

    [..!..] Main Menu

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.Audio;
+using UnityEngine.UI;
+
+public class GameManager : MonoBehaviour
+{
+    public static bool SG = false;
+    public static bool AR = false;
+    public static bool SR = false;
+    public static bool gamePaused = false;
+    public AudioMixer audioMixer;
+    public Slider musicSlider;
+    public Slider sfxSlider;
+    private GameManager gameManager;
+    void Start()
+    {
+        if(gameManager == null)
+        {
+            gameManager = this;
+        }
+        else
+        {
+            Destroy(this);
+        }
+    }
+
+    void Update()
+    {
+        
+    }
+
+    public void setShotgun()
+    {
+        SG = true;
+        AR = false;
+        SR = false;
+    }
+
+    public void setAR()
+    {
+        SG = false;
+        AR = true;
+        SR = false;
+    }
+
+    public void setSR()
+    {
+        SG = false;
+        AR = false;
+        SR = true;
+    }
+
+    public void setSFXVolume()
+    {
+        audioMixer.SetFloat("SFXVolume", sfxSlider.value);
+        Debug.Log(sfxSlider.value);
+    }
+
+    public void setMusicVolume()
+    {
+        audioMixer.SetFloat("MusicVolume", musicSlider.value);
+        Debug.Log(musicSlider.value);
+    }
+}
---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MainMenu : MonoBehaviour
+{
+    public GameObject mainMenu;
+    public GameObject optionsMenu;
+    public GameObject loadoutMenu;
+    public GameObject gameManager;
+    //public GameObject htpMenu;
+    //public GameObject creditsMenu;
+
+    void Update()
+    {
+        
+    }
+
+    public void StartGame()
+    {
+        mainMenu.SetActive(false);
+        loadoutMenu.SetActive(true);
+    }
+
+    public void Options()
+    {
+        mainMenu.SetActive(false);
+        optionsMenu.SetActive(true);
+    }
+
+    public void Quit()
+    {
+        Application.Quit();
+    }
+
+    public void HowToPlay()
+    {
+       
+    }
+
+    public void Credits()
+    {
+
+    }
+
+    public void optionsBack()
+    {
+        optionsMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void startBack()
+    {
+        loadoutMenu.SetActive(false);
+        mainMenu.SetActive(true);
+    }
+
+    public void Play()
+    {
+        SceneManager.LoadScene("ParkourLevel");
+    }
+}
